6d8c3e49a43b9bdd07c835fdfc7459a7bba463da
Apoorava singh <109285079+Apoorv-sr@users.noreply.github.com>
2022-08-31 14:58:23 +0000

    completed Create a Model

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose=require("mongoose");
 const mySecret = process.env['MONGO_URI'];
 
-mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mySecret,{ useNewUrlParser: true,useUnifiedTopology: true});
 
 // console.log(mySecret);
 
@@ -15,10 +15,47 @@ let personSchema=new mongoose.Schema(
 
 let Person=new mongoose.model("Person",personSchema);
 
+let person1=new Person(
+  {
+  name:"Arup",
+  age:26
+  }
+);
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+
+ person1.save(function(err,data)
+             {
+               if(err){
+                 console.log(err);
+               }
+               else
+               {
+                 console.log("Successfully saved");
+               }
+             });
+
+const createAndSavePerson = function(done) {
+  // ... do something (risky) ...the abve function done created is executed only when the content //inside it is successfully executed.
+  person1.save(function(err,data)
+             {
+               if(err){
+                 console.log(err);
+                 return;
+               }
+               else
+               {
+                 console.log("Successfully saved");
+                 done(null,data);
+               }
+             });
+  
+  if (error) return done(error);
+  done(null, result);
 };
+
+// const createAndSavePerson = (done) => {
+//   done(null /*, data*/);
+// };
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
